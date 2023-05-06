13a48e409ff7fa514b016f008f74fccc929c5e2e
Mohamed-Mokrani <med.mokranii@hotmail.fr>
2023-05-06 17:24:38 +0100

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import movie from "../../Assets/Projects/movie-time.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -23,12 +23,12 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={movie}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink=""
-              
+              title="Movie Time"
+              description="
+              Discover your next favorite movie with our website! Search our database for trailers, reviews, and ratings, and find all the info you need on cast, crew, and release dates. Plus, stay up-to-date on movie news and join the discussion. Find your perfect flick today!"
+              ghLink="https://github.com/Mohamed-Mokrani/Movie-App"
             />
           </Col>
 
@@ -39,7 +39,6 @@ function Projects() {
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink=""
-              
             />
           </Col>
 
@@ -50,7 +49,6 @@ function Projects() {
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink=""
-              
             />
           </Col>
 
@@ -61,7 +59,6 @@ function Projects() {
               title="Plant AI"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
               ghLink=""
-              
             />
           </Col>
 
